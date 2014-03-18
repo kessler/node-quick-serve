ffc3831749553554032d96ceb8ced12ea10cc6fb
Yaniv Kessler <yanivk@gmail.com>
2014-03-18 12:42:44 +0200

    initial

---
+++ README.md
@@ -0,0 +1,17 @@
+# quick serve
+A simple command line tool to quickly set up an http server that serves one file
+
+```
+npm install quick-serve
+```
+
+```
+quickserve myfile
+```
+myfile will be served on http://localhost:12345
+
+or with a custom port
+```
+quickserve myfile 8181
+```
+myfile will be served on http://localhost:8181
---
+++ index.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+var fs = require('fs')
+var http = require('http')
+var path = require('path')
+var mime = require('mime')
+
+var filename = process.argv[2]
+var port = process.argv[3] || 12345
+
+if (!filename)
+	throw new Error('must provide a file name')
+
+var realFilename = path.resolve(process.cwd(), filename)
+
+var contentType = mime.lookup(realFilename)
+
+var server = http.createServer(function(request, response) {
+
+	response.setHeader('content-type', contentType)
+	fs.createReadStream(realFilename).pipe(response)
+
+}).listen(port, function (err) {
+	if (err) return console.error(err)
+
+	console.log('serving %s on http://localhost:%s', realFilename, port)
+})
+
+
+
